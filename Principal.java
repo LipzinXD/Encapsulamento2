5a4f97f25c23740e8508a51e64fc235c0dbc9287
LipzinXD <felipescamargo806@gmail.com>
2023-04-11 14:06:31 +0000

    Encapsulamento

---
+++ Principal.java
@@ -0,0 +1,15 @@
+public class Principal {
+    public static void mian(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Felipe");
+        System.out.println(pessoa.getNome());
+        
+        pessoa.setIdade(18);
+        System.out.println(pessoa.getIdade());
+
+        pessoa.setCelular("(18)99634-1508");
+        System.out.println(pessoa.getCelular());
+        pessoa.setCpf("493.236.238-22");
+        System.out.println(pessoa.getCpf());
+    }
+}
